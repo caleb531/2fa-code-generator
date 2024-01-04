a7b718f480e84976d6a56cd81ce98cbb3c480e56
Caleb Evans <caleb@calebevans.me>
2024-01-03 17:33:04 -0800

    Get countdown and auto-refresh functional
    The app is fully functional at this point. We just need to polish it.

--- src/routes/Code.svelte
+++ src/routes/Code.svelte
@@ -1,13 +1,33 @@
 <script lang="ts">
 	import { getOTPLib } from '$lib';
+	import { onMount } from 'svelte';
+
+	// The number of seconds a 2FA code will last before we must generate a new
+	// code
+	const TIME_STEP = 30;
+	// The number of milliseconds to wait before re-rendering the countdown or
+	// checking if the code is still valid
+	const VERIFY_DELAY = 500;
 
 	export let secret: string;
 	let code: string;
+	let timeRemaining: number;
+
+	function calculateTimeRemaining() {
+		return TIME_STEP - ((Date.now() / 1000) % TIME_STEP);
+	}
 
 	async function generateCode(secret: string) {
 		const { totp } = await getOTPLib();
 		code = totp.generate(secret);
+		timeRemaining = calculateTimeRemaining();
 	}
+
+	onMount(() => {
+		setInterval(() => {
+			generateCode(secret);
+		}, VERIFY_DELAY);
+	});
 
 	$: {
 		if (typeof window !== 'undefined') {
@@ -16,10 +36,19 @@
 	}
 </script>
 
-<p class="code-container">
+<div class="code-container">
 	{#if code}
-		<span class="code">{code}</span>
+		<div class="code">{code}</div>
 	{:else}
-		<span class="loading">Loading...</span>
+		<div class="loading">Loading...</div>
 	{/if}
-</p>
+	<div class="code-countdown" class:visible={timeRemaining !== undefined}>
+		<div class="code-countdown-bar-wrapper">
+			<div
+				class="code-countdown-bar"
+				style:--percent-time-remaining={Math.ceil(timeRemaining) / TIME_STEP}
+			></div>
+		</div>
+		<div class="code-countdown-count">{Math.ceil(timeRemaining)}</div>
+	</div>
+</div>
